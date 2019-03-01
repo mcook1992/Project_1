f886731cb2eefc8f9fbe82fab71ba79dd6126219
Kira Helberg <kira.helberg@gmail.com>
2019-02-28 22:05:35 -0500

    Added carousel images, updated font, updated CSS

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,25 @@
 "use strict";
 
-alert("We have contact");
+$(".carousel").carousel();
+
+// Image Carousel
+// var images = [
+//   "assets/images/beach.jpg",
+//   "assets/images/snow-mountain.jpg",
+//   "assets/images/autumn-leaves.jpg"
+// ];
+
+// var showImage;
+// var count = 0;
+
+// function displayImage() {
+//   $("#carouselImages").html("<img src=" + images[count] + " width='100%'>");
+//   count++;
+//   if (count === images.length) {
+//     count = 0;
+//   }
+// }
+
+// displayImage();
+
+// showImage = setInterval(displayImage, 10000);
