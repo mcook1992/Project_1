b5129278ade29a60ab69c4f4a7989df9cd83e154
Joseph Cannova <jcannova@wheelership.com>
2019-03-01 18:04:25 -0500

    Added listener to option selector, returned selected value, made a request to API based on selection, changed ajax call into a function to allow multiple uses

--- assets/aeris.js
+++ assets/aeris.js
@@ -1,27 +1,9 @@
+/**
+ * Different enpoints that were tested
+ */
 
 
-// window.onload = function() {
-//
-//     /**
-//      * STEP 2
-//      * Set your account access keys when the page and scripts have loaded.
-//      */
-//     const aeris = new AerisWeather('bvpLuTRRLs5tMbTcMqmhm', '0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj');
-//
-//     console.log(aeris.clientId);
-//     /**
-//      * STEP 3
-//      * Start using the library by accessing the available convenience methods on your `AerisWeather` instance.
-//      */
-//     aeris.api().endpoint('normals').radius('10miles').get().then((result) => {
-//         console.log("DEBUG:", result);
-//         const data = result.data.ob;
-//         console.log((data));
-//     });
-//
-// };
-
-const aerisApiBeginFixed = 'https://api.aerisapi.com';
+/** Enpoint Observation Returning temp closests to Zip 10027 and sorting temp on response*/
 //const endPoint = '/observations/closest?p=10027&search?query=sort=temp:&limit=25' + '&';
 
 // const endPoint = '/observations/search?query=sort=temp:-1&limit=25' + '&';
@@ -34,22 +16,40 @@ const aerisApiBeginFixed = 'https://api.aerisapi.com';
 
 // const endPoint = '/observations/search?query=temp:21.1&sort=temp:-1&limit=250&';
 
-const endPoint = '/observations/search?query=temp:42.76&limit=250&';
-
-
-const apiCredentials = 'client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj';
+//const endPoint = '/observations/search?query=temp:42.76&limit=250&';
 
 //const queryUrl = 'https://api.aerisapi.com/normals/closest?p=:auto&radius=100000miles&limit=100000&client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj';
 
-const queryUrl = aerisApiBeginFixed + endPoint + apiCredentials;
-console.log(queryUrl);
 
-$.ajax({
-    url: queryUrl,
-    method: "GET"
-}).then(function (response){
-    console.log(response);
-}).catch(function(error){
-    console.log(error);
-});
+
+
+
+
+/********
+*Aeris API Request Function is a reusable Ajax call to Aeris API
+*@params tempParam is looking for a temperature (in Celsius) and will add it into the query to be sent to Aeris.
+ */
+function aerisAPIRequest (tempParam) {
+    //API URL beginning - Shouldn't Change
+    const aerisApiBeginFixed = 'https://api.aerisapi.com';
+    //API Credentials - Shouldn't Change
+    const apiCredentials = 'client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj';
+
+    //Params part of the API Query
+    const endPoint = '/observations/search?query=temp:'+ tempParam + '&limit=250&';
+
+    //Building the full query
+    const queryUrl = aerisApiBeginFixed + endPoint + apiCredentials;
+
+    //Begin Ajax request using jQuery
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    }).catch(function (error) {
+        console.log(error);
+    });
+
+}
 
--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,25 @@
 "use strict";
 
 //alert("We have contact");
+
+//Listening for a dropdown option to be selected
+$('#sel1').on('change', function() {
+
+    //Selected value is passed as a parameter to a switch
+    switch(parseInt(this.value)){
+
+        case 1:
+            aerisAPIRequest(21.77);
+            break;
+        case 2:
+            aerisAPIRequest(15);
+            break;
+        case 3:
+            aerisAPIRequest(8);
+            break;
+        case 4:
+            aerisAPIRequest(0);
+
+    }
+
+});
