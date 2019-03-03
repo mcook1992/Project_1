4ea361bf18c3d312d2e7ed9ebade4242d7c16fe6
Kira Helberg <kira.helberg@gmail.com>
2019-03-03 13:40:48 -0500

    Changed origin to dropdown + updated css

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var departureCity;
+var departureCity = $("#origin").val();
 
 //Create sample array with results
 var results = [
@@ -40,7 +40,10 @@ function getResults() {
     $("#searchResults").append(resultsDiv);
   }
 }
-getResults();
+$("#searchBtn").on("click", function(event) {
+  event.preventDefault();
+  getResults();
+});
 
 //alert("We have contact");
 
@@ -50,48 +53,44 @@ var maxTemp = 0;
 
 //Listening for a dropdown option to be selected
 
-$('#searchDropdown').on('change', function() {
+$("#searchDropdown").on("change", function() {
+  //Taking the value of the User Selection and parsing it into an Int
+  var userInput = parseInt(this.value);
 
-    //Taking the value of the User Selection and parsing it into an Int
-    var userInput = parseInt(this.value);
-  
-    //Setting Min Max For each Category
-    if(userInput === 1){
-        maxTemp = 35;
-    }
-    if(userInput === 2){
-        maxTemp = 23.8889;
-    }
-    if(userInput === 3){
-        maxTemp = 18.3333;
-    }
-    if(userInput === 4){
-        maxTemp = 1.67;
-    }
-    if(userInput === 5){
-        maxTemp = -7.77778;
-    }
+  //Setting Min Max For each Category
+  if (userInput === 1) {
+    maxTemp = 35;
+  }
+  if (userInput === 2) {
+    maxTemp = 23.8889;
+  }
+  if (userInput === 3) {
+    maxTemp = 18.3333;
+  }
+  if (userInput === 4) {
+    maxTemp = 1.67;
+  }
+  if (userInput === 5) {
+    maxTemp = -7.77778;
+  }
 
-    //Selected value is passed as a parameter to a switch
-    switch(userInput){
-
-        case 1:
-            aerisAPIRequest(26.67, -1, maxTemp);
-            closestAirport("New York");
-            break;
-        case 2:
-            aerisAPIRequest(21, 9, maxTemp);
-            break;
-        case 3:
-            aerisAPIRequest(12.78,999, maxTemp);
-            break;
-        case 4:
-            aerisAPIRequest(-1.1, 999, maxTemp);
-        case 5:
-            aerisAPIRequest(-12.22, 999, maxTemp);
-
-    }
-
+  //Selected value is passed as a parameter to a switch
+  switch (userInput) {
+    case 1:
+      aerisAPIRequest(26.67, -1, maxTemp);
+      closestAirport("New York");
+      break;
+    case 2:
+      aerisAPIRequest(21, 9, maxTemp);
+      break;
+    case 3:
+      aerisAPIRequest(12.78, 999, maxTemp);
+      break;
+    case 4:
+      aerisAPIRequest(-1.1, 999, maxTemp);
+    case 5:
+      aerisAPIRequest(-12.22, 999, maxTemp);
+  }
 });
 
 //confirm whether user actually typed a city
