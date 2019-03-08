f68c628a0521423ada109af7883a45651664ec62
Vertigo-1 <46118156+Vertigo-1@users.noreply.github.com>
2019-03-07 22:45:39 -0500

    Add files via upload

--- assets/aeris.js
+++ assets/aeris.js
@@ -1,132 +1,172 @@
-/********
- *Aeris API Request Function returns a list of citys, temp and cheap flights.
- *@params tempParam is looking for a temperature (in Celsius) and will add it into the query to be sent to Aeris.
- * @params sortParam will allow us to return a sorted array from Aeris
- * @params maxTemp is used to remove places that are hotter than temp tange
- * @params is the departure city
+/**
+ * Different enpoints that were tested
  */
-function aerisAPIRequest (tempParam, sortParam, maxTemp, OriginID) {
 
-    //Clearing array on every request
-    arrayOfPlaces = [];
-    cityResults= [];
+/** Enpoint Observation Returning temp closests to Zip 10027 and sorting temp on response*/
+//const endPoint = '/observations/closest?p=10027&search?query=sort=temp:&limit=25' + '&';
 
-    <!--BUILDING AERIS URL REQUEST -->
-    //API URL beginning - Shouldn't Change
-    const aerisApiBeginFixed = 'https://api.aerisapi.com';
-    //API Credentials - Shouldn't Change
-    const apiCredentials = 'client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj';
+// const endPoint = '/observations/search?query=sort=temp:-1&limit=25' + '&';
 
-    //Params part of the API Query
-    const endPoint = '/observations/search?query=temp:'+ tempParam + '&sort=temp:'+ sortParam + '&limit=250&';
+//const endPoint = '/observations/search?query=country:us&sort=temp:-1&limit=25&';
 
-    //Building the full query
-    const queryUrl = aerisApiBeginFixed + endPoint + apiCredentials;
+//const endPoint = '/normals/search?query=tavg:75&sort=temp:-1&limit=25&';
 
+// const endPoint = '/observations/within?p=10027&query=temp:21.1&radius=3000mi&limit=100&';
 
+// const endPoint = '/observations/search?query=temp:21.1&sort=temp:-1&limit=250&';
 
-    //Begin Ajax request
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function (response) {
+//const endPoint = '/observations/search?query=temp:42.76&limit=250&';
 
-        const cities = sortResults(response, maxTemp);
-        
-         cities.forEach(({City: cityName, Temp: temp, Country: country}) => {
-             const airportRequestURI = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/"+country+"/USD/en-US/?query=" + cityName.replace(" ", "+");
-             skyScanner.get(airportRequestURI)
-                 .then((res) => {
-                     if(res.data.Places[0].PlaceId !== undefined) {
-                         const destinationAirportID = res.data.Places[0].PlaceId;
-                         const cheapestFlightsURI = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${OriginID}/`+ destinationAirportID + "/2019-10-01?inboundpartialdate=2019-11-01"
-                         skyScanner.get(cheapestFlightsURI)
-                             .then(res => {
+//const queryUrl = 'https://api.aerisapi.com/normals/closest?p=:auto&radius=100000miles&limit=100000&client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj';
 
-                                 const {Places: places, Quotes: quotes} = res.data;
+//Initilaizing Global Array Var of Places
+var arrayOfPlaces = [];
 
-                                 places.length > 1 && quotes.length > 0 ? console.log(`FLIGHT: ${places[1].IataCode} to ${places[0].IataCode} @ $${quotes[0].MinPrice} on ${quotes[0].QuoteDateTime} on ${country}`) : null;
+const skyScanner = axios.create({
+  headers: {
+    get: {
+      "X-RapidAPI-Key": "32546dec3amsh296808ab8e0c6f4p1dd857jsn160dd89c68ad"
+    }
+  }
+});
 
-                                 if(places.length > 1 && quotes.length > 0) {
-                                     var createDisplayObject;
-                                     if(places[0].IataCode === "JFK" ||
-                                        places[0].IataCode === "LGA" ||
-                                        places[0].IataCode === "EWR") {
-                                         createDisplayObject = {
-                                             cityName: places[1].CityName,
-                                             price: "$" + quotes[0].MinPrice,
-                                             temp: temp
-                                         };
+/********
+ *Aeris API Request Function is a reusable Ajax call to Aeris API
+ *@params tempParam is looking for a temperature (in Celsius) and will add it into the query to be sent to Aeris.
+ */
 
-                                         cityResults.push(createDisplayObject);
-                                     }else{
-                                         createDisplayObject = {
-                                             cityName: places[0].CityName,
-                                             price: "$" + quotes[0].MinPrice,
-                                             temp: temp
-                                         };
+function aerisAPIRequest(tempParam, sortParam, maxTemp, OriginID) {
+  //API URL beginning - Shouldn't Change
+  const aerisApiBeginFixed = "https://api.aerisapi.com";
+  //API Credentials - Shouldn't Change
+  const apiCredentials =
+    "client_id=bvpLuTRRLs5tMbTcMqmhm&client_secret=0Bpm48kezpufXqLX6UzBvpBSfkGG4zOH6b6CuUwj";
 
-                                         cityResults.push(createDisplayObject);
-                                     }
+  //Params part of the API Query
+  const endPoint =
+    "/observations/search?query=temp:" +
+    tempParam +
+    "&sort=temp:" +
+    sortParam +
+    "&limit=250&";
 
+  //Building the full query
+  const queryUrl = aerisApiBeginFixed + endPoint + apiCredentials;
 
-                                 }
+  //Begin Ajax request using jQuery
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  })
+    .then(function(response) {
+      //console.log(response.response[0].ob.tempF);
+      //console.log(response)
+      const cities = sortResults(response, maxTemp);
+      cities.forEach(({ City: cityName }) => {
+        const airportRequestURI =
+          "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/US/USD/en-US/?query=" +
+          cityName.replace(" ", "+");
+        skyScanner
+          .get(airportRequestURI)
+          .then(res => {
+            if (res.data.Places[0].PlaceId !== undefined) {
+              const destinationAirportID = res.data.Places[0].PlaceId;
+              const cheapestFlightsURI =
+                `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/${OriginID}/` +
+                destinationAirportID +
+                "/2019-10-01?inboundpartialdate=2019-11-01";
+              skyScanner.get(cheapestFlightsURI).then(res => {
+                console.log(res);
+                const { Places: places, Quotes: quotes } = res.data;
 
-                             })
-                     }
-                 })
-                 .catch()
-         })
-    }).done((res) => { setTimeout(createImageResults,40000); setTimeout(getResults,50000)}).catch(function (error) {
+                places.length > 1 && quotes.length > 0
+                  ? console.log(
+                      `FLIGHT: ${places[1].IataCode} to ${
+                        places[0].IataCode
+                      } @ $${quotes[0].MinPrice} on ${quotes[0].QuoteDateTime}`
+                    )
+                  : null;
 
+                if (places.length > 1 && quotes.length > 0) {
+                  var createDisplayObject;
+                  if (
+                    places[0].IataCode === "JFK" ||
+                    places[0].IataCode === "LGA" ||
+                    places[0].IataCode === "EWR"
+                  ) {
+                    createDisplayObject = {
+                      cityName: places[1].CityName,
+                      price: "$" + quotes[0].MinPrice,
+                      temp: 70
+                    };
 
+                    cityResults.push(createDisplayObject);
+                  } else {
+                    createDisplayObject = {
+                      cityName: places[0].CityName,
+                      price: "$" + quotes[0].MinPrice,
+                      temp: 70
+                    };
+
+                    cityResults.push(createDisplayObject);
+                  }
+                }
+              });
+            }
+          })
+          .catch();
+      });
+    })
+    .done(res => {
+      console.log("DEBUG: HIT THE LAST THEN");
+      setTimeout(createImageResults, 30000);
+      setTimeout(getResults, 40000);
+    })
+    .catch(function(error) {
+      //console.log(error);
     });
 }
 
-/**
- * Sorts and removes temps
- * @param result
- * @param maxTemp
- * @returns {Array}
- */
-function sortResults (result, maxTemp){
-var counter = 0;
-arrayOfPlaces = [];
-    for(var i = 0; i<result.response.length; i++){
-
-     if (counter < 250){
-         if (result.response[i].ob.tempC < maxTemp) {
-             var newPlacesObject = {
-                 id: i,
-                 Temp: result.response[i].ob.tempF,
-                 City: result.response[i].place.name,
-                 Country: result.response[i].place.country
-             };
-             var index = newPlacesObject.City.indexOf("/");
-             if (index > 0){
-                 newPlacesObject.City = newPlacesObject.City.substring(0,index);
-             }
-             var parantStringTest = newPlacesObject.City.indexOf("(");
-             if(parantStringTest > 0){
-                 newPlacesObject.City = newPlacesObject.City.substring(0, parantStringTest);
-             }
-             var airportStringTest = newPlacesObject.City.indexOf("air");
-             if(airportStringTest >0){
-                 newPlacesObject.City = newPlacesObject.City.substring(0, airportStringTest);
-             }
-             arrayOfPlaces.push(newPlacesObject);
-             counter++;
-
-         }
-
-     }
-
-
-
+function sortResults(result, maxTemp) {
+  var counter = 0;
+  arrayOfPlaces = [];
+  for (var i = 0; i < result.response.length; i++) {
+    if (counter < 250) {
+      if (result.response[i].ob.tempC < maxTemp) {
+        var newPlacesObject = {
+          id: i,
+          Temp: result.response[i].ob.tempF,
+          City: result.response[i].place.name,
+          Country: result.response[i].place.country
+        };
+        var index = newPlacesObject.City.indexOf("/");
+        if (index > 0) {
+          newPlacesObject.City = newPlacesObject.City.substring(0, index);
+        }
+        var parantStringTest = newPlacesObject.City.indexOf("(");
+        if (parantStringTest > 0) {
+          newPlacesObject.City = newPlacesObject.City.substring(
+            0,
+            parantStringTest
+          );
+        }
+        var airportStringTest = newPlacesObject.City.indexOf("air");
+        if (airportStringTest > 0) {
+          newPlacesObject.City = newPlacesObject.City.substring(
+            0,
+            airportStringTest
+          );
+        }
+        arrayOfPlaces.push(newPlacesObject);
+        counter++;
+      }
     }
-    return arrayOfPlaces;
 
-
+    //result.response[i].ob.tempC
+    //look at temp ...if greater than max move on else less store in array
+  }
+  return arrayOfPlaces;
+  // closestAirportByArray(arrayOfPlaces);
 }
 
-
+//Teddy needs country and city
--- assets/main.js
+++ assets/main.js
@@ -1,19 +1,14 @@
 "use strict";
 
-<!-- Declaring Global Variables -->
+var departureCity = $("#origin").val();
+var userTempSelect = $("#temp").val();
 
-var departureCity = $("#origin").val(); //Stores departure city chosen
-var arrayOfPlaces = []; //Stores an array of places, temp, city, countrys
-const skyScanner = axios.create({
-    headers: {
-        get: {
-            "X-RapidAPI-Key": "32546dec3amsh296808ab8e0c6f4p1dd857jsn160dd89c68ad"
-        }
-    }
-});
-let cityResults = []; //Returns Cities based on Temp
-let imageResults = []; //Returns images based on city
-
+//unsplash image results API and URLs
+//let cityResults = ["London", "New York", "Paris", "San Juan", "Honolulu"];
+let cityResults = [];
+let imageResults = [];
+// Generic function for capturing the tag name from the data-attribute
+//createImageResults();
 
 function createImageResults() {
   //for loop to get array of flyTags into data
@@ -31,18 +26,26 @@ function createImageResults() {
     });
   }
   console.log(imageResults);
+
+  // image loading fade in from left to right with 1.5 second delay
+  // $(imageResults)
+  //   .hide()
+  //   .show("slide", { direction: "left" }, 2500);
 }
 //displays results: runs through 4 arrays for city, image, temp, price, to create results blocks
 function getResults() {
+  console.log("DEBUG: INSIDE GETRESULTS FUNCTION FROM CALLBACK");
 
   for (var i = 0; i < cityResults.length; i++) {
     var resultsDiv = $("<div class='col-lg-4 col-md-6 results-container'>");
-    
+
     var resultImage = $("<img class='resultImage'/>");
     resultImage.attr("src", imageResults[i]);
     resultImage.attr("alt", cityResults[i].cityName);
 
-    var resultCity = $("<p id='resultsLocation'>").text(cityResults[i].cityName);
+    var resultCity = $("<p id='resultsLocation'>").text(
+      cityResults[i].cityName
+    );
     var resultTemp = $("<p id='resultsTemp'>").text(cityResults[i].temp);
     var resultFlight = $("<p id='resultsFlight'>").text(cityResults[i].price);
 
@@ -50,8 +53,6 @@ function getResults() {
     resultsDiv.append(resultCity);
     resultsDiv.append(resultTemp);
     resultsDiv.append(resultFlight);
-
-   
 
     var favoriteButton = $("<button class='favoriteButton'>");
     favoriteButton.text("Favorite");
@@ -63,10 +64,9 @@ function getResults() {
 
     resultsDiv.append(resultImage);
     resultsDiv.append(resultCity);
-//     resultsDiv.append(resultTemp);
-//     resultsDiv.append(resultFlight);
+    //     resultsDiv.append(resultTemp);
+    //     resultsDiv.append(resultFlight);
     resultsDiv.append(favoriteButton);
-
 
     $("#searchResults").append(resultsDiv);
   }
@@ -74,6 +74,19 @@ function getResults() {
 $("#searchBtn").on("click", function(event) {
   event.preventDefault();
   //getResults();
+
+  //scroll down by 500 pixels on search button click
+  //window.scrollTo(0, 0, 0, 500);
+
+  //button press shows loading gif until success
+  // $('#buttonID').click(function(){
+  //   $('#img').show(); //<----here
+  //   $.ajax({
+  //     ??? not sure if needed
+  //    success:function(result){
+  //        $('#img').hide();  //<--- hide again
+  //    }
+  // }
 });
 
 //this is related to the flight stuff
@@ -82,66 +95,58 @@ var maxTemp = 0;
 
 //Listening for a dropdown option to be selected
 
-$('#searchBtn').on("click", function(evt) {
+$("#searchBtn").on("click", function(evt) {
+  var userInput = parseInt($("#temp").val());
+  //Taking the value of the User Selection and parsing it into an Int
+  var departureCity = $("#origin").val();
+  console.log(userInput);
+  //Setting Min Max For each Category
+  if (userInput === 1) {
+    maxTemp = 35;
+  }
+  if (userInput === 2) {
+    maxTemp = 23.8889;
+  }
+  if (userInput === 3) {
+    maxTemp = 18.3333;
+  }
+  if (userInput === 4) {
+    maxTemp = 1.67;
+  }
+  if (userInput === 5) {
+    maxTemp = -7.77778;
+  }
 
-    var userInput = parseInt($("#temp").val());
-    //Taking the value of the User Selection and parsing it into an Int
-    var departureCity = $("#origin").val();
-
-    //Setting Min Max For each Category
-    if (userInput === 1) {
-        maxTemp = 35;
-    }
-    if (userInput === 2) {
-        maxTemp = 23.8889;
-    }
-    if (userInput === 3) {
-        maxTemp = 18.3333;
-    }
-    if (userInput === 4) {
-        maxTemp = 1.67;
-    }
-    if (userInput === 5) {
-        maxTemp = -7.77778;
-    }
-
-    //Selected value is passed as a parameter to a switch
-    switch (userInput) {
-
-        case 1:
-            $("#searchResults").empty();
-            imageResults = [];
-            aerisAPIRequest(26.67, -1, maxTemp, departureCity);
-            break;
-        case 2:
-            $("#searchResults").empty();
-            imageResults = [];
-            aerisAPIRequest(21, 9, maxTemp, departureCity);
-            break;
-        case 3:
-            $("#searchResults").empty();
-            imageResults = [];
-            aerisAPIRequest(12.78, 999, maxTemp, departureCity);
-            break;
-        case 4:
-            $("#searchResults").empty();
-            imageResults = [];
-            aerisAPIRequest(-1.1, 999, maxTemp, departureCity);
-        case 5:
-            $("#searchResults").empty();
-            imageResults = [];
-            aerisAPIRequest(-12.22, 999, maxTemp, departureCity);
-
-    }
-
-})
+  //Selected value is passed as a parameter to a switch
+  switch (userInput) {
+    case 1:
+      aerisAPIRequest(26.67, -1, maxTemp, departureCity);
+      break;
+    case 2:
+      aerisAPIRequest(21, 9, maxTemp, departureCity);
+      break;
+    case 3:
+      aerisAPIRequest(12.78, 999, maxTemp, departureCity);
+      break;
+    case 4:
+      aerisAPIRequest(-1.1, 999, maxTemp, departureCity);
+    case 5:
+      aerisAPIRequest(-12.22, 999, maxTemp, departureCity);
+  }
+});
 
 //confirm whether user actually typed a city
 
 $("#searchBtn").on("click", function(event) {
   event.preventDefault();
   departureCity = $("#origin").val();
+  console.log(departureCity);
 
+  //USE THIS CODE FOR WHEN WE CAN'T GET A RESULT
+  //   var newDiv = $("<div>");
+  //   var varText = $("<p>").text("We could not find any results");
+  //   newDiv.append(varText);
+  //   $("#searchResults").prepend(newDiv);
 });
 
 //Hot = 80-95
@@ -150,3 +155,4 @@ $("#searchBtn").on("click", function(event) {
 //cold = 30-35
 //Freezing = 10-18
 
+//TODO; Sort data and exclude items Display on frontend
