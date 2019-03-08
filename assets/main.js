7c12b219899451ff7f28041c3b0c6e143f155a17
Kira Helberg <kira.helberg@gmail.com>
2019-03-07 22:07:33 -0500

    trying to sort images

--- assets/main.js
+++ assets/main.js
@@ -3,13 +3,11 @@
 var departureCity = $("#origin").val();
 var userTempSelect = $("#temp").val();
 
-
 //unsplash image results API and URLs
-//let cityResults = ["London", "New York", "Paris", "San Juan", "Honolulu"];
 let cityResults = [];
 let imageResults = [];
 // Generic function for capturing the tag name from the data-attribute
-//createImageResults();
+//let imageResultsCity = [];
 
 function createImageResults() {
   //for loop to get array of flyTags into data
@@ -23,23 +21,39 @@ function createImageResults() {
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      imageResults.push(response.results["0"].urls.regular);
+      // imageResults.push(response.results["0"].urls.regular);
+
+      var imageResultObject = {
+        indexValue: i,
+        cityImageUrl: response.results["0"].urls.regular,
+        cityName: imageData
+      };
+
+      imageResults.push(imageResultObject);
     });
   }
-  console.log(imageResults);
+
+  imageResults.sort(function(a, b) {
+    return a[indexValue] - b[indexValue];
+  });
+
+  console.log(typeof imageResults);
 }
+
 //displays results: runs through 4 arrays for city, image, temp, price, to create results blocks
 function getResults() {
-    console.log("DEBUG: INSIDE GETRESULTS FUNCTION FROM CALLBACK");
+  console.log("DEBUG: INSIDE GETRESULTS FUNCTION FROM CALLBACK");
 
   for (var i = 0; i < cityResults.length; i++) {
     var resultsDiv = $("<div class='col-lg-4 col-md-6 results-container'>");
-    
+
     var resultImage = $("<img class='resultImage'/>");
     resultImage.attr("src", imageResults[i]);
     resultImage.attr("alt", cityResults[i].cityName);
 
-    var resultCity = $("<p id='resultsLocation'>").text(cityResults[i].cityName);
+    var resultCity = $("<p id='resultsLocation'>").text(
+      cityResults[i].cityName
+    );
     var resultTemp = $("<p id='resultsTemp'>").text(cityResults[i].temp);
     var resultFlight = $("<p id='resultsFlight'>").text(cityResults[i].price);
 
@@ -47,8 +61,6 @@ function getResults() {
     resultsDiv.append(resultCity);
     resultsDiv.append(resultTemp);
     resultsDiv.append(resultFlight);
-
-   
 
     var favoriteButton = $("<button class='favoriteButton'>");
     favoriteButton.text("Favorite");
@@ -58,19 +70,13 @@ function getResults() {
     favoriteButton.attr("imageURL", imageResults[i]);
     favoriteButton.attr("class", "btn btn-default favoriteBTN");
 
-    resultsDiv.append(resultImage);
-    resultsDiv.append(resultCity);
-//     resultsDiv.append(resultTemp);
-//     resultsDiv.append(resultFlight);
     resultsDiv.append(favoriteButton);
-
 
     $("#searchResults").append(resultsDiv);
   }
 }
 $("#searchBtn").on("click", function(event) {
   event.preventDefault();
-  //getResults();
 });
 
 //this is related to the flight stuff
@@ -79,68 +85,44 @@ var maxTemp = 0;
 
 //Listening for a dropdown option to be selected
 
-$('#searchBtn').on("click", function(evt) {
+$("#searchBtn").on("click", function(evt) {
+  var userInput = parseInt($("#temp").val());
 
-    var userInput = parseInt($("#temp").val());
-    //Taking the value of the User Selection and parsing it into an Int
-    var departureCity = $("#origin").val();
-    console.log(userInput);
-    //Setting Min Max For each Category
-    if (userInput === 1) {
-        maxTemp = 35;
-    }
-    if (userInput === 2) {
-        maxTemp = 23.8889;
-    }
-    if (userInput === 3) {
-        maxTemp = 18.3333;
-    }
-    if (userInput === 4) {
-        maxTemp = 1.67;
-    }
-    if (userInput === 5) {
-        maxTemp = -7.77778;
-    }
+  //Taking the value of the User Selection and parsing it into an Int
+  var departureCity = $("#origin").val();
+  console.log(userInput);
 
-    //Selected value is passed as a parameter to a switch
-    switch (userInput) {
+  //Setting Min Max For each Category
+  if (userInput === 1) {
+    maxTemp = 35;
+  }
+  if (userInput === 2) {
+    maxTemp = 23.8889;
+  }
+  if (userInput === 3) {
+    maxTemp = 18.3333;
+  }
+  if (userInput === 4) {
+    maxTemp = 1.67;
+  }
+  if (userInput === 5) {
+    maxTemp = -7.77778;
+  }
 
-        case 1:
-            aerisAPIRequest(26.67, -1, maxTemp, departureCity);
-            break;
-        case 2:
-            aerisAPIRequest(21, 9, maxTemp, departureCity);
-            break;
-        case 3:
-            aerisAPIRequest(12.78, 999, maxTemp, departureCity);
-            break;
-        case 4:
-            aerisAPIRequest(-1.1, 999, maxTemp, departureCity);
-        case 5:
-            aerisAPIRequest(-12.22, 999, maxTemp, departureCity);
-
-    }
-
-})
-
-//confirm whether user actually typed a city
-
-$("#searchBtn").on("click", function(event) {
-  event.preventDefault();
-  departureCity = $("#origin").val();
-  console.log(departureCity);
-
-  //USE THIS CODE FOR WHEN WE CAN'T GET A RESULT
-  //   var newDiv = $("<div>");
-  //   var varText = $("<p>").text("We could not find any results");
-  //   newDiv.append(varText);
-  //   $("#searchResults").prepend(newDiv);
+  //Selected value is passed as a parameter to a switch
+  switch (userInput) {
+    case 1:
+      aerisAPIRequest(26.67, -1, maxTemp, departureCity);
+      break;
+    case 2:
+      aerisAPIRequest(21, 9, maxTemp, departureCity);
+      break;
+    case 3:
+      aerisAPIRequest(12.78, 999, maxTemp, departureCity);
+      break;
+    case 4:
+      aerisAPIRequest(-1.1, 999, maxTemp, departureCity);
+    case 5:
+      aerisAPIRequest(-12.22, 999, maxTemp, departureCity);
+  }
 });
-
-//Hot = 80-95
-//Warm = 70-75
-//cool = 55-65
-//cold = 30-35
-//Freezing = 10-18
-
-//TODO; Sort data and exclude items Display on frontend
